9f8894a699fa652cc7b75a839620f39fd62f2517
krupalsinhchavda <krupalsinh.chavda.epistic@gmail.com>
2024-12-12 16:47:05 +0530

    live database

--- config/database.js
+++ config/database.js
@@ -1,40 +1,40 @@
-require('dotenv').config();
-
-const mysql = require('mysql');
-const dbconnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'tech_edu'
-});
-dbconnection.connect(function (err) {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log("Connected to database!");
-    }
-});
-
-module.exports = dbconnection;
-
 // require('dotenv').config();
 
 // const mysql = require('mysql');
 // const dbconnection = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     port: process.env.DB_PORT || 3306  // Default to 3306 if DB_PORT is not specified
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'tech_edu'
 // });
-
 // dbconnection.connect(function (err) {
 //     if (err) {
 //         console.log(err);
-//     } else {
+//     }
+//     else {
 //         console.log("Connected to database!");
 //     }
 // });
 
 // module.exports = dbconnection;
+
+require('dotenv').config();
+
+const mysql = require('mysql');
+const dbconnection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306  // Default to 3306 if DB_PORT is not specified
+});
+
+dbconnection.connect(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Connected to database!");
+    }
+});
+
+module.exports = dbconnection;
